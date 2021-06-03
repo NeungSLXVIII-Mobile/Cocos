797979d91e1b88eadb0df6e0f6899a0c1a4fa36f
NeungSLXVIII-Mobile <neungslxviii.mobile@gmail.com>
2021-06-04 06:31:33 +0700

    ...

--- v2/SonOfVayuSurf/src/settings.1b46f.js
+++ v2/SonOfVayuSurf/src/settings.1b46f.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD:v2/SonOfVayuSurf/src/settings.1b46f.js
 window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Intro.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"01588",main:"a78b4"}};
-=======
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Intro.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"01588",main:"91302"}};
->>>>>>> 438d000f45b51de431e57b9ec325ad9075ea13a1:v2/SonOfVayuSurf/src/settings.4a93f.js
