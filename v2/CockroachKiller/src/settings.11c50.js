f2eff25d6e7c59bf5db20a29c1021bca3a0e6e1a
NeungSLXVIII-Mobile <neungslxviii.mobile@gmail.com>
2021-06-05 17:20:53 +0700

    ...

--- v2/CockroachKiller/src/settings.11c50.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD:v2/CockroachKiller/src/settings.11c50.js
-window._CCSettings={platform:"web-mobile",groupList:["default","UI"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Intro.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"01588",main:"e27a0"}};
-=======
-window._CCSettings={platform:"web-mobile",groupList:["default","UI"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Intro.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"01588",main:"f9cd3"}};
->>>>>>> 7939ea37d8e626a3f0fb358ee58c3c3c3033dda1:v2/CockroachKiller/src/settings.58ad7.js
