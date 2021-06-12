59d6eff40343049cb213af5ed5e96f2bb461faf6
NeungSLXVIII-Mobile <neungslxviii.mobile@gmail.com>
2021-06-12 23:08:15 +0700

    ...

--- v2/SeaBassKiller/src/settings.6d03e.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Intro.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"01588",main:"c3847"}};
